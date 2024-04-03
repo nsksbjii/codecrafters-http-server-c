dee903747024524814bfd56b1da9c419eb4a528e
ketrptr <ketrptr@cocaine.ninja>
2024-04-03 17:41:02 +0200

    stage 7

--- app/main.c
+++ app/main.c
@@ -4,8 +4,11 @@
 int main(int argc, char **argv) {
   if (argc != 3) {
     printf("useage:\n %s --directory PATH\n", argv[0]);
-    return 1;
+    server("/home/ketrptr/code/codecrafters/httpServerC/"
+           "codecrafters-http-server-c/");
+    return 0;
   }
 
   server(argv[2]);
+  return 0;
 }
